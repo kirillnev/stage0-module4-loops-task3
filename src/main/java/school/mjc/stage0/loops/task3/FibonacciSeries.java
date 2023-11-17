7806bf8077d369bc3344d40c672c135fa13a6271
kirillnev <kirillnev84@gmail.com>
2023-11-17 19:22:15 +0600

    done

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        if (lastFibonacci <= 0) {
+            return;
+        }
+
+        int first = 0, second = 1;
+        for (int i = 1; i <= lastFibonacci; i++) {
+            System.out.println(first);
+            int next = first + second;
+            first = second;
+            second = next;
+        }
     }
 }
